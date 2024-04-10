390b3d7ed56afc1668d66717ad73d80d0494158a
Jaroslav Langer <langeja5@fit.cvut.cz>
2024-04-10 11:32:45 +0200

    code: class NN and body of raf function taken from <https://github.com/YanasGH/RAFs/blob/main/main_experiments/rafs.py> * Model Protocol

--- code/nncmaes.py
+++ code/nncmaes.py
@@ -6,6 +6,7 @@ import math
 from numbers import Real
 import os
 import random
+from random import randint
 import sys
 import time
 import types
@@ -13,6 +14,8 @@ from typing import Callable, Optional, Protocol, Type
 
 import numpy as np
 import numpy.typing as npt
+import tensorflow  # type: ignore[import-untyped]
+import tensorflow.compat.v1 as tf  # type: ignore[import-untyped]
 
 with suppress(ImportError):
     import cocoex  # type: ignore
@@ -249,15 +252,8 @@ class ProblemIoh(Problem):
         return self.__problem.optimum.y + 1e-8 >= self.__problem.state.current_best.y
 
     @property
-    def progress(self) -> float:
-        optimum, curr_best = self.__problem.optimum.y, self.current_best_y
-        if optimum >= 0:
-            if curr_best == 0:
-                return 1
-            else:
-                return optimum / curr_best
-        else:
-            return curr_best / optimum
+    def delta_to_optimum(self) -> float:
+        return self.current_best_y - self.__problem.optimum.y
 
 
 class Cma(ABC):
@@ -577,9 +573,174 @@ def shift_and_scale_x_by_es_y_by_train(*, x_train, y_train, x_test, es):
     return (es.mean, es.std), get_mean_and_std(y_train)
 
 
-def predict_zeros(*, x_train, y_train, x_test, **ignored):
+class Model(Protocol):
+    def __call__(
+        self,
+        *,
+        x_train: npt.NDArray[np.float64],
+        y_train: npt.NDArray[np.float64],
+        x_test: npt.NDArray[np.float64],
+    ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]: ...
+
+
+def predict_zeros(
+    *,
+    x_train: npt.NDArray[np.float64],
+    y_train: npt.NDArray[np.float64],
+    x_test: npt.NDArray[np.float64],
+) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
     zeros = np.zeros((x_test.shape[0], y_train.shape[-1]))
     return zeros, zeros
+
+# https://github.com/YanasGH/RAFs/blob/main/main_experiments/rafs.py#L15-L91
+class NN():
+    def __init__(self, x_dim, y_dim, hidden_size, init_stddev_1_w, init_stddev_1_b, 
+                 init_stddev_2_w, n, learning_rate, ens):
+
+        # setting up as for a usual NN
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.hidden_size = hidden_size 
+        self.n = n
+        self.learning_rate = learning_rate
+        
+        # set up NN
+        self.inputs = tf.placeholder(tf.float64, [None, x_dim], name='inputs')
+        self.y_target = tf.placeholder(tf.float64, [None, y_dim], name='target')
+        
+        activation_fns = [tensorflow.keras.activations.selu, tf.nn.tanh, tensorflow.keras.activations.gelu, tensorflow.keras.activations.softsign, tf.math.erf, tf.nn.swish, tensorflow.keras.activations.linear]
+        
+        if ens <= len(activation_fns)-1:
+            self.layer_1_w = tf.layers.Dense(hidden_size,
+            activation = activation_fns[ens],
+            kernel_initializer=tf.random_normal_initializer(mean=0.,stddev=init_stddev_1_w),
+            bias_initializer=tf.random_normal_initializer(mean=0.,stddev=init_stddev_1_b))
+        else:
+            af_ind = randint(0,3)
+            self.layer_1_w = tf.layers.Dense(hidden_size,
+            activation= activation_fns[af_ind],
+            kernel_initializer=tf.random_normal_initializer(mean=0.,stddev=init_stddev_1_w),
+            bias_initializer=tf.random_normal_initializer(mean=0.,stddev=init_stddev_1_b))
+        
+        self.layer_1 = self.layer_1_w.apply(self.inputs)
+        
+        self.output_w = tf.layers.Dense(y_dim, 
+            activation=None, use_bias=False,
+            kernel_initializer=tf.random_normal_initializer(mean=0.,stddev=init_stddev_2_w))
+        
+        self.output = self.output_w.apply(self.layer_1)
+        
+        # set up loss and optimiser - this is modified later with anchoring regularisation
+        self.opt_method = tf.train.AdamOptimizer(self.learning_rate)
+        self.mse_ = 1/tf.shape(self.inputs, out_type=tf.int64)[0] * tf.reduce_sum(tf.square(self.y_target - self.output))
+        self.loss_ = 1/tf.shape(self.inputs, out_type=tf.int64)[0] * tf.reduce_sum(tf.square(self.y_target - self.output))
+        self.optimizer = self.opt_method.minimize(self.loss_)
+        return
+    
+    
+    def get_weights(self, sess):
+        '''method to return current params'''
+        
+        ops = [self.layer_1_w.kernel, self.layer_1_w.bias, self.output_w.kernel]
+        w1, b1, w2 = sess.run(ops)
+        
+        return w1, b1, w2 
+    
+    
+    def anchor(self, sess, lambda_):   #lambda_anchor
+        '''regularise around initial parameters''' 
+        
+        w1, b1, w2 = self.get_weights(sess)
+
+        # get initial params
+        self.w1_init, self.b1_init, self.w2_init = w1, b1, w2 
+        
+        loss = lambda_[0]*tf.reduce_sum(tf.square(self.w1_init - self.layer_1_w.kernel))
+        loss += lambda_[1]*tf.reduce_sum(tf.square(self.b1_init - self.layer_1_w.bias))
+        loss += lambda_[2]*tf.reduce_sum(tf.square(self.w2_init - self.output_w.kernel))
+        
+        # combine with original loss
+        self.loss_ = self.loss_ + 1/tf.shape(self.inputs, out_type=tf.int64)[0] * loss 
+        self.optimizer = self.opt_method.minimize(self.loss_)
+        return
+      
+    def predict(self, x, sess):
+        '''predict method'''
+        
+        feed = {self.inputs: x}
+        y_pred = sess.run(self.output, feed_dict=feed)
+        return y_pred
+# https://github.com/YanasGH/RAFs/blob/main/main_experiments/rafs.py#L15-L91
+
+def raf(*, x_train: npt.NDArray[np.float64], y_train: npt.NDArray[np.float64], x_test: npt.NDArray[np.float64]):
+    X_train, y_train, X_val = x_train, y_train, x_test
+    # https://github.com/YanasGH/RAFs/blob/main/main_experiments/rafs.py#L94-L157
+    n = X_train.shape[0]
+    x_dim = X_train.shape[1]
+    y_dim = y_train.shape[1]
+
+    n_ensembles = 5
+    hidden_size = 100
+    init_stddev_1_w =  np.sqrt(10)
+    init_stddev_1_b = init_stddev_1_w # set these equal
+    init_stddev_2_w = 1.0/np.sqrt(hidden_size) # normal scaling
+    data_noise = 0.01 #estimated noise variance, feel free to experiment with different values
+    lambda_anchor = data_noise/(np.array([init_stddev_1_w,init_stddev_1_b,init_stddev_2_w])**2)
+
+    n_epochs = 1000
+    learning_rate = 0.01
+
+    NNs=[]
+    y_prior=[]
+    tf.reset_default_graph()
+    sess = tf.Session()
+
+    # loop to initialise all ensemble members, get priors
+    for ens in range(0,n_ensembles):
+        NNs.append(NN(x_dim, y_dim, hidden_size, 
+                      init_stddev_1_w, init_stddev_1_b, init_stddev_2_w, n, learning_rate, ens))
+        
+        # initialise only unitialized variables - stops overwriting ensembles already created
+        global_vars = tf.global_variables()
+        is_not_initialized   = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+        not_initialized_vars = [v for (v, f) in zip(global_vars, is_not_initialized) if not f]
+        if len(not_initialized_vars):
+            sess.run(tf.variables_initializer(not_initialized_vars))
+        
+        # do regularisation now that we've created initialisations
+        NNs[ens].anchor(sess, lambda_anchor)  #Do that if you want to minimize the anchored loss
+        
+        # save their priors
+        y_prior.append(NNs[ens].predict(X_val, sess))
+
+    for ens in range(0,n_ensembles):
+        
+        feed_b = {}
+        feed_b[NNs[ens].inputs] = X_train
+        feed_b[NNs[ens].y_target] = y_train
+        print('\nNN:',ens)
+        
+        ep_ = 0
+        while ep_ < n_epochs:    
+            ep_ += 1
+            blank = sess.run(NNs[ens].optimizer, feed_dict=feed_b)
+            if ep_ % (n_epochs/5) == 0:
+                loss_mse = sess.run(NNs[ens].mse_, feed_dict=feed_b)
+                loss_anch = sess.run(NNs[ens].loss_, feed_dict=feed_b)
+                print('epoch:', ep_, ', mse_', np.round(loss_mse*1e3,3), ', loss_anch', np.round(loss_anch*1e3,3))
+                # the anchored loss is minimized, but it's useful to keep an eye on mse too
+
+    # run predictions
+    y_pred=[]
+    for ens in range(0,n_ensembles):
+        y_pred.append(NNs[ens].predict(X_val, sess))
+
+    """Display results:"""
+
+    method_means = np.mean(np.array(y_pred)[:,:,0], axis=0).reshape(-1,)
+    method_stds = np.sqrt(np.square(np.std(np.array(y_pred)[:,:,0],axis=0, ddof=1)) + data_noise).reshape(-1,)
+    # https://github.com/YanasGH/RAFs/blob/main/main_experiments/rafs.py#L94-L157
+    return method_means[:, None], method_stds[:, None]
 
 
 class SurrogateCallable(Protocol):
@@ -594,7 +755,7 @@ class SurrogateCallable(Protocol):
 
 
 class Surrogate:
-    def __init__(self, *, model, subset, shift_and_scale):
+    def __init__(self, *, model: Model, subset: Subset, shift_and_scale):
         self.__model = model
         self.__subset = subset
         self.__shift_and_scale = shift_and_scale
@@ -647,8 +808,8 @@ class EvolutionControl(Protocol):
 
 def evaluate_all(
     *,
-    pred: Optional[npt.NDArray[np.float64]] = None,
-    pred_std: Optional[npt.NDArray[np.float64]] = None,
+    pred: npt.NDArray[np.float64],
+    pred_std: npt.NDArray[np.float64],
     points: npt.NDArray[np.float64],
     problem: Problem,
 ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.uint]]:
@@ -731,7 +892,9 @@ def seek_minimum(
                 pred=pred, pred_std=pred_std, points=points, problem=problem
             )
         else:
-            values, eval_idx = evaluate_all(points=points, problem=problem)
+            values, eval_idx = evaluate_all(
+                pred=np.array([]), pred_std=np.array([]), points=points, problem=problem
+            )
 
         x_archive = np.concatenate((x_archive, points[eval_idx]))
         y_archive = np.concatenate((y_archive, values[eval_idx]))
@@ -766,7 +929,11 @@ def report(*, problem, es, surrogate, ec, n_min, secs, seed) -> str:
             f"{problem.evaluations}-evals",
             f"{es.evals}-cma-evals",
             f"{secs:>4.0f}s",
-            *([f"{problem.progress:.8%}"] if hasattr(problem, "progress") else []),
+            *(
+                [f"{problem.delta_to_optimum:.1e}-delta-f"]
+                if hasattr(problem, "delta_to_optimum")
+                else []
+            ),
             f"{str(problem.final_target_hit):>5}-solved",
             f"{es.restarts}-restarts",
             f"{seed:>10}-seed",
@@ -783,14 +950,16 @@ def set_seed(seed: int = 42, verbose=False) -> None:
         torch.cuda.manual_seed(seed)  # type: ignore[name-defined] # noqa: F821
         torch.backends.cudnn.deterministic = True  # type: ignore[name-defined] # noqa: F821
         torch.backends.cudnn.benchmark = False  # type: ignore[name-defined] # noqa: F821
-    # # tf.random.set_seed(seed) # module 'tensorflow._api.v2.compat.v1.random' has no attribute 'set_seed'
-    # tensorflow.random.set_seed(seed)
-    # tf.random.set_random_seed(seed)
-    # tensorflow.experimental.numpy.random.seed(seed) # module 'tensorflow._api.v2.compat.v1.experimental' has no attribute 'numpy'
-    # tf.set_random_seed(seed)
+    # tf.random.set_seed(seed) # module 'tensorflow._api.v2.compat.v1.random' has no attribute 'set_seed'
+    tensorflow.random.set_seed(seed)
+    tf.random.set_random_seed(seed)
+    tensorflow.experimental.numpy.random.seed(
+        seed
+    )  # module 'tensorflow._api.v2.compat.v1.experimental' has no attribute 'numpy'
+    tf.set_random_seed(seed)
     # When running on the CuDNN backend, two further options must be set
-    # os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
-    # os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
+    # os.environ['TF_DETERMINISTIC_OPS'] = '1' # Random ops require a seed to be set when determinism is enabled. Please set a seed before running the op, e.g. by calling tf.random.set_seed(1).
     # Set a fixed value for the hash seed
     os.environ["PYTHONHASHSEED"] = str(seed)
     if verbose:
@@ -803,15 +972,19 @@ def get_seed_np() -> np.uint32:
 
 
 if __name__ == "__main__":
-    for problem_class in [ProblemCocoex, ProblemIoh]:
-        problem = problem_class.get(dim=2, fun=2, inst=1, budget_coef=250 * 10**0)
+    for problem_class in [ProblemCocoex, ProblemIoh][1:]:
+        dim, fun, inst, budget_coef = 2, 1, 1, 250
+        problem = problem_class.get(
+            dim=dim, fun=fun, inst=inst, budget_coef=budget_coef
+        )
         set_seed()
+        tf.compat.v1.disable_eager_execution()
         seed = get_seed_np()
         x, y = seek_minimum(
             problem,
             es=WrappedCma(x0=np.zeros(problem.dimension), seed=seed),
             surrogate=Surrogate(
-                model=predict_zeros,
+                model=raf,
                 subset=ClosestToEachTestPoint(n_max_coef=20, norm=Mahalanobis),
                 shift_and_scale=shift_and_scale_x_by_es_y_by_train,
             ),
@@ -820,3 +993,10 @@ if __name__ == "__main__":
             seed=seed,
             log=lambda **kwargs: print(report(**kwargs)),
         )
+        if SAVE := True:
+            import pickle
+
+            with open(
+                f"d{dim:0>2}_f{fun:0>2}_i{inst:0>2}_b{budget_coef:0>3}_x_y.pickle", "wb"
+            ) as f:
+                pickle.dump((x, y), f, protocol=pickle.HIGHEST_PROTOCOL)
