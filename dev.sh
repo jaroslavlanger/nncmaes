a77f95ec9165187f74f25a1521667b71c6c7271f
Jaroslav Langer <langeja5@fit.cvut.cz>
2024-04-13 21:59:20 +0200

    dev.sh - 'python -m compileall -q' added to the check function.

--- dev.sh
+++ dev.sh
@@ -1,8 +1,15 @@
 check () {
-    python -m ruff check --fix $1
-    python -m mypy $1
-    python -m pyright $1
-    python -m doctest $1
+    (
+        if [ $# -ne 1 ]; then
+            echo "Wrong number of arguments! Use: 'check <module>'." >&2
+            exit 1
+        fi
+        python -m compileall -q $1 \
+        && python -m ruff check --fix $1 \
+        && python -m mypy $1 \
+        && python -m pyright $1 \
+        && python -m doctest $1
+    )
 }
 
 format () {
